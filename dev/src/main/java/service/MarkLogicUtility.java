e98aad3ee26b9d05d3b21d72e7740e7abeb38937
Armand Prévot <armand-prevot@hotmail.fr>
2021-02-12 20:27:45 +0100

    DataUtility

---
+++ dev/src/main/java/service/MarkLogicUtility.java
@@ -0,0 +1,31 @@
+package service;
+
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.DatabaseClientFactory;
+import com.marklogic.client.pojo.PojoRepository;
+
+public class MarkLogicUtility {
+
+    public DatabaseClient client;
+    public String host;
+    public int port;
+    public String username;
+    public String password;
+
+    public MarkLogicUtility() {
+
+        this.host = "localhost";
+        this.port = 8001;
+        this.username = "Admin";
+        this.password = "Admin";
+        this.createClient(host, port, username, password);
+
+    }
+
+    public void createClient(String host, int port, String username, String password) {
+
+        this.client = DatabaseClientFactory.newClient(host, port, new DatabaseClientFactory.DigestAuthContext(username, password));
+
+    }
+    
+}
